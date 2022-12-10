5709435390f5e5e1afddd077e694d0c30b159b89
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2022-12-10 17:00:16 +0530

    add dotnet

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
@@ -17,8 +18,7 @@ const userSchema= new mongoose.Schema({
     password: String
 })
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt,{secret: secret , encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret: process.env.SECRET , encryptedFields:["password"]});
 
 const User = mongoose.model("user",userSchema)
 
